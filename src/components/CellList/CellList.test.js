d0768d3d11147eb64d52dd4afb6ea8e1b935ba83
metamn <bartus.csongor@gmail.com>
2018-12-02 12:14:09 +0200

    Adding tests

--- src/components/CellList/CellList.test.js
+++ src/components/CellList/CellList.test.js
@@ -7,10 +7,34 @@ import 'jest-styled-components'
 import CellList from './CellList';
 
 describe('CellList', function() {
+	it('should support mixed units like `%` for width, `px` for height', () => {
+		const wrapper = renderer.create(<CellList width={'30%'} height={'300px'} numberOfElements={5} />).toJSON();
+		expect(wrapper).toHaveStyleRule('height', '300px');
+		expect(wrapper).toHaveStyleRule('width', '30%');
+	});
+
+	it('should support `px` units', () => {
+		const wrapper = renderer.create(<CellList height={'300px'} numberOfElements={5} />).toJSON();
+		expect(wrapper).toHaveStyleRule('height', '300px');
+	});
+
+	it('should support `%` units', () => {
+		const wrapper = renderer.create(<CellList height={'10%'} numberOfElements={5} />).toJSON();
+		expect(wrapper).toHaveStyleRule('height', '10%');
+	});
+
+	it('should support `vh` units', () => {
+		const wrapper = renderer.create(<CellList height={'10vh'} numberOfElements={5} />).toJSON();
+		expect(wrapper).toHaveStyleRule('height', '10vh');
+	});
+
+	it('should support `vh` units', () => {
+		const wrapper = renderer.create(<CellList height={'10vh'} numberOfElements={5} />).toJSON();
+		expect(wrapper).toHaveStyleRule('height', '10vh');
+	});
+
 	it('should support `em` units', () => {
-		const wrapper = renderer.create(
-			<CellList width={'10em'} numberOfElements={5} />
-		).toJSON();
+		const wrapper = renderer.create(<CellList width={'10em'} numberOfElements={5} />).toJSON();
 		expect(wrapper).toHaveStyleRule('width', '10em');
 	});
 });
