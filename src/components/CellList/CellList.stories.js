4ba4fc40f4dd5e738c4aa0f837e9ffe458e1add8
metamn <bartus.csongor@gmail.com>
2018-12-02 19:03:34 +0200

    Refactoring and documenting

--- src/components/CellList/CellList.stories.js
+++ src/components/CellList/CellList.stories.js
@@ -6,9 +6,11 @@ import '@storybook/addon-console';
 
 import CellList from './CellList';
 
+
 const stories = storiesOf('Cell List', module);
 
 stories.add('empty', () => <CellList width={'10vw'} />);
+
 stories.add('loading', () => <CellList width={'10vw'} numberOfElements={5} loading={true} />);
 
 stories.add('single line - horizontal', () => {
@@ -28,13 +30,16 @@ stories.add('single line - vertical', () => {
 });
 
 stories.add('single line - horizontal 2', () => <CellList width={'90vw'} height={'20vh'} numberOfElements={5} />)
+
 stories.add('single line - vertical 2', () => <CellList width={'29vw'} height={'80vh'} numberOfElements={12} />)
+
 stories.add('square', () => <CellList width={'30vw'} height={'30vh'} numberOfElements={15} />)
+
 stories.add(
-	  'rectangle - horizontal',
-	  () => <CellList width={'400px'} height={'33vh'} numberOfElements={91} />,
+	  'rectangle - horizontal', () => <CellList width={'400px'} height={'33vh'} numberOfElements={91} />,
 	  {
 		  notes: 'To Fix: with `px` things got truncated',
 	  }
   	)
+
 stories.add('rectangle - vertical', () => <CellList width={'30%'} height={'80%'} numberOfElements={121} />)
